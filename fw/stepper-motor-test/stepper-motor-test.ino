c592d422b349e9d202b932645ce7c74f827591cc
ImogenWren <imogen.wren91@gmail.com>
2024-12-18 16:54:49 +0000

    bfdf

--- fw/stepper-motor-test/stepper-motor-test.ino
+++ fw/stepper-motor-test/stepper-motor-test.ino
@@ -7,17 +7,17 @@ MPU6050 mpu;
 autoDelay printDelay;
 autoDelay stallDelay;
 
-#define PRINT_RATE_Hz 5
+#define PRINT_RATE_Hz 40
 #define PRINT_PERIODIC_UPDATES true
 
 #define HALL_SENSOR_PIN A4
-#define HALL_SENSOR_LOW_TRIGGER 295   // home state is triggered when hall sensor returns lower than this value
-#define HALL_SENSOR_HIGH_TRIGGER 700  // estimate for backwards magnet
+//#define HALL_SENSOR_LOW_TRIGGER 295   // home state is triggered when hall sensor returns lower than this value
+//#define HALL_SENSOR_HIGH_TRIGGER 700  // estimate for backwards magnet
 
 #define HALL_NORMALLY_HIGH true  // define if normally high, triggered by low pulse (true) or normally low triggered by high pulse (false)
 
 #define ENCODE_RAW_ANGLE_OFFSET 0.0
-#define STEPPER_HOLD_CURRENT 0
+#define STEPPER_HOLD_CURRENT 10
 
 #define COMMAND_SIZE 64
 
@@ -25,10 +25,13 @@ autoDelay stallDelay;
 // Stall count limit never reaches above 5 as position changes just enough to clear stall warning
 #define STALL_COUNT_LIMIT 5  // Limit for typical number of stall events before triggering stall reset behaviour
 // Variables to track total number of stalls and limit users to a defined number per time period
-#define STALL_OPPORTUNITIES 3    // number of times stall reset behaivour can be triggered before motor is limited
-#define STALL_COOL_DOWN_PERIOD 15   // cool down period to reset the number of triggered stalls
+#define STALL_OPPORTUNITIES 3      // number of times stall reset behaivour can be triggered before motor is limited
+#define STALL_COOL_DOWN_PERIOD 15  // cool down period to reset the number of triggered stalls
 // When limit is reached, automatic stall guard is implemented by limiting how long the motor can stay in stall condition
-#define PROTECT_STALL_COUNT_LIMIT 2   // Limit for number of stall events that trigger stall reset behaviour if protection mode has been activated
+#define PROTECT_STALL_COUNT_LIMIT 2  // Limit for number of stall events that trigger stall reset behaviour if protection mode has been activated
+
+#define MAX_MOTOR_STEPS_S 800
+#define MAX_MOTOR_ACC_STEPS_S_S 4000
 
 bool ping_mode = false;  // temporary flag to keep data output while doing a servo "ping"
 
@@ -87,7 +90,7 @@ uint16_t current_stall_limit = STALL_COUNT_LIMIT;  // this variable is used by c
 
 // do one full revolution to find the lowest value reported by the hall effect sensor
 void find_home() {
-  Serial.println("Finding Home");
+  // Serial.println("Finding Home");
 
   int16_t hall_sensor_val;
   bool finding_center = true;
@@ -113,28 +116,37 @@ void find_home() {
       Serial.println(low_angle);
     }
     if (stepper.angleMoved() >= 360 || stepper.angleMoved() <= -360) {
-      Serial.println("ending init rotation");
+      //     Serial.println("ending init rotation");
       finding_center = false;
     }
   }
 }
 
+#define HOMING_TIMEOUT_S 10
 // Use the previously found hall effect sensor low reading to return the mass to the center position
-void move_home() {
+int16_t move_home() {
+  uint32_t start_time_mS = millis();
   bool home_found = false;
   int16_t hall_sensor_val;
-  Serial.println("Moving Home, please stand by..");
+  //  Serial.println("Moving Home, please stand by..");
   while (!home_found) {
-    hall_sensor_val = analogRead(HALL_SENSOR_PIN);
-    //  Serial.println(hall_sensor_val);
-    if (hall_sensor_val == low_point) {
+    hall_sensor_val = analogRead(HALL_SENSOR_PIN);  // measure the hall sensor
+                                                    // Serial.println(hall_sensor_val);
+    // timeout clause here to exit loop if home cannot be found (two functions can be written into their own loop with the output of this function as the escape clause)
+    if (millis() - start_time_mS >= HOMING_TIMEOUT_S * 1000) {
+      Serial.println("move_home() timeout");
+      /// Serial.println("Moving to Angle 0");
+      //stepper.moveToAngle(0);  //
+      return 0;
+    }
+
+    if (hall_sensor_val == low_point) {  // when the low point is found again, run homing sequence
       stepper.stop();
       home_found = true;
       Serial.print("Home Found, Angle: ");
       Serial.println(stepper.encoder.getAngleRaw());
       stepper.moveAngle(180);  // this method seems to be under performing by factor of 10!
       while (stepper.getMotorState(POSITION_REACHED)) {
-        // Serial.println("Moving to Positon");
         ;  // wait for motor to reach position
       }
       stepper.encoder.setHome();
@@ -148,18 +160,30 @@ void move_home() {
       stepper.setRPM(60);
     }
   }
+  return 1;  // on success return 1
 }
 
-float encode_rpm;
-float encode_pos;
+// wraps previous two functions together into a reliable structure
+void run_calibration() {
+  bool cal_complete;
+  while (!cal_complete) {
+    find_home();
+    cal_complete = move_home();
+  }
+  stepper.setMaxVelocity(MAX_MOTOR_STEPS_S);
+  stepper.setMaxAcceleration(MAX_MOTOR_ACC_STEPS_S_S);
+}
+
+//float encode_rpm;
+//float encode_pos;
 // Function to track stall status
 void check_stall_status() {
-  encode_rpm = stepper.encoder.getRPM();
-  encode_pos = stepper.encoder.getAngle();
+  float encode_rpm = stepper.encoder.getRPM();
+  float encode_pos = stepper.encoder.getAngle();
   uint16_t current_encoder_pos = stepper.encoder.getAngleRaw();
   if (current_encoder_pos == last_encoder_pos && rpm != 0) {
     stallCount++;
-    if (stallCount > 1) {
+    if (stallCount > 2) {
       Serial.print("Motor Stall Detected No: ");  // detection of momentary stall does not trigger stall protection behaviour
       Serial.println(stallCount);
     }
@@ -203,20 +227,18 @@ void setup() {
   Serial.begin(115200);
   delay(100);  // for stability
   Serial.println("\nuStepper S32 - Test");
-  // delay(1000);  // for UI
+  delay(1000);  // for UI
 
   mpu.Initialize();                                                   // Initialization of MPU
   mpu.Calibrate();                                                    // Calibration - LED will blink until calibration is done !
                                                                       // delay(3000);                                                     // for calibration
   stepper.setup(NORMAL, 400, 10, 0.2, 0.0, 16, true, false, 100, 1);  //Initialize uStepper S32
-  //stepper.setCurrent(100);     // set motor current as percentage not useable unless current jumper placed in I-PWM position
-  stepper.setHoldCurrent(STEPPER_HOLD_CURRENT);  // set holding current as percentage
+  stepper.setCurrent(100);                                            // set motor current as percentage not useable unless current jumper placed in I-PWM position
+  stepper.setHoldCurrent(STEPPER_HOLD_CURRENT);                       // set holding current as percentage
 
-  //  stepper.enableStallguard(0, false);  // (threshold (-64 to + 63), stop_on_stall, RPM?) // default 4  // this is useless
+  // stepper.enableStallguard(0, false);  // (threshold (-64 to + 63), stop_on_stall, RPM?) // default 4  // this is useless
 
-  find_home();
-  move_home();
-
+  run_calibration();
 
   Serial.print("Current Encoder Pos (raw): ");
   Serial.println(stepper.encoder.getAngleRaw());
@@ -254,13 +276,17 @@ void loop() {
     Serial.println(value);
 
     // Parse command
-    if (value == 0) {
+
+    if (value == -100) {  // hack case to trigger a servo ping
+
+    } else if (value == 0) {
       stepper.stop(HARD);
       stepper.setRPM(0);
-      Serial.println("stop cmd received");
+      //   Serial.println("stop cmd received");
       Hz = 0;
       rpm = 0;
-      move_home();
+      //move_home();
+      run_calibration();
     } else if (value > 0 || value < 0) {
       Hz = value;
       rpm = get_RPM_from_Hz(Hz);
@@ -270,10 +296,10 @@ void loop() {
 
 
   // Function to track stall status
-  check_stall_status();
+  // check_stall_status();
 
   // Function to track total stalls per period
-  stall_manager();
+  // stall_manager();
 
 
 
@@ -287,9 +313,12 @@ void loop() {
   // Program Text Output
 
   if (rpm > 0.0 || rpm < 0.0 || ping_mode) {  // If the motor has commanded movement, or ping mode is active
-    if (printDelay.millisDelay(print_delay_mS)) {
+ //   if (printDelay.millisDelay(print_delay_mS)) {
+if (printDelay.secondsDelay(5)) {
       // if (Xacc == 0){
       // float actualRPM = stepper.getDriverRPM();
+      float encode_rpm = stepper.encoder.getRPM();
+      float encode_pos = stepper.encoder.getAngle();
       float actualHz = encode_rpm / 60.0;
       char printBuffer[200];
       char pos_buf[8];
@@ -325,8 +354,10 @@ void loop() {
       dtostrf(encode_rpm, 2, 2, a_rpm_buf);
 
       sprintf(printBuffer, "pos: %6s ,set_Hz: %6s, rep_Hz: %6s, set_RPM: %6s, rep_rpm: %6s, acc: %6s, %6s, %6s, gyro: %6s, %6s, %6s ", pos_buf, hz_buf, a_hz_buf, rpm_buf, a_rpm_buf, accX_buf, accY_buf, accZ_buf, gyroX_buf, gyroY_buf, gyroZ_buf);
+      //sprintf(printBuffer, "%6s, %6s, %6s, %6s, %6s, %6s, %6s ", pos_buf, accX_buf, accY_buf, accZ_buf, gyroX_buf, gyroY_buf, gyroZ_buf);
+      // sprintf(printBuffer, "%6s, %6s, %6s,", accX_buf, accY_buf, accZ_buf);
 #if PRINT_PERIODIC_UPDATES == true
-      Serial.println(printBuffer);
+        Serial.println(printBuffer);
 #endif
     }
   }
