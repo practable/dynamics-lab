08ffae8710c9cb37f1f06e529a38f70ecd656157
Imogen Wren <imogen.wren91@gmail.com>
2025-02-04 16:40:47 +0000

    servo example with new library

--- fw/servo-test/servo-test.ino
+++ fw/servo-test/servo-test.ino
@@ -1,13 +1,18 @@
 
-#include <Servo.h>
+//#include <Servo.h>
+#include <NewServo.h> // Available @ https://github.com/GhassanYusuf/NewServo
 #include <autoDelay.h>
 
 autoDelay servoDelay;
 
-Servo servo;  // create servo object to control a servo
+//Servo servo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
+NewServo servo(6);
+
 int pos = 0;  // variable to store the servo position
+
+#define COMMAND_SIZE 64
 
 #define SERVO_PPM_PIN 6
 
@@ -15,32 +20,55 @@ int pos = 0;  // variable to store the servo position
 #define SERVO_OPEN_POS 20
 
 void setup() {
-  servo.attach(SERVO_PPM_PIN);
-  servo.write(0);
+  servo.begin();
+  servo.setInit(-90);
+  servo.setMin(-90);
+  servo.setMax(20);
+
+  // with current settings passing 0 and -90 causes it to go full distance
+
+  /// servo.attach(SERVO_PPM_PIN);
+  // servo.write(0);
   delay(1000);
 }
 
 int16_t hall_sensor_value;
 
-int angle_addr = 5;
+int angle;
 
 int servo_target = 0;
 
 int microseconds;
 
+
+
 void loop() {
 
-  servo.write(servo_target);
-  microseconds = servo.read();
-  Serial.println(microseconds);
+  // Get Serial Message
+  if (Serial.available() > 0) {
+    float value;
+    char command[COMMAND_SIZE];
+    Serial.readBytesUntil(10, command, COMMAND_SIZE);
+    Serial.print("\ncmd: ");
+    Serial.println(command);
+    Serial.println();
 
-  if (servoDelay.secondsDelay(5)) {
-    if (servo_target == SERVO_OPEN_POS) {
-      servo_target = SERVO_ZERO_POS;
 
-    } else if (servo_target == SERVO_ZERO_POS) {
-      servo_target = SERVO_OPEN_POS;
 
-    }
+
+    value = atof(command);
+    Serial.print("data: ");
+    Serial.println(value);
+    servo_target = int(value);
+    // servo.write(servo_target);
+    Serial.print("target: ");
+    Serial.println(servo_target);
+    servo.move(servo_target);
   }
+  // angle = servo.read();
+  // microseconds = servo.readMicroseconds();
+  // Serial.print("angle: ");
+  // Serial.print(angle);
+  // Serial.print(" mS: ");
+  // Serial.println(microseconds);
 }
